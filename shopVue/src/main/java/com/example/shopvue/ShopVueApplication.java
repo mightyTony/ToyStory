739a92311b35577895df837086cf6de2fa3e4da4
tony <mightytonykr@gmail.com>
2022-12-05 19:45:43 +0900

    vue toy

---
+++ shopVue/src/main/java/com/example/shopvue/ShopVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopVueApplication.class, args);
+    }
+
+}
